b2ccbf2fb5898aa414a5176ad506cd317b15732f
harryFBloch <harryfbloch@gmail.com>
2018-10-12 12:07:33 -0400

    scraped links from coins

--- lib/satoshi/scraper.rb
+++ lib/satoshi/scraper.rb
@@ -1,13 +1,14 @@
 class Satoshi::Scraper
-
+  COIN_MARKET_CAP_URL = "https://coinmarketcap.com/"
 
   def self.scrape_top_100_coins
-      doc = Nokogiri::HTML(open("https://coinmarketcap.com/"))
+      doc = Nokogiri::HTML(open(COIN_MARKET_CAP_URL))
        coin_array = doc.css("tbody tr").each_with_index.map { |coin_container, index|
          name = coin_container.css("a.currency-name-container").text.strip
+         link = coin_container.css("a.currency-name-container").attr("href").value
          price = coin_container.css("a.price").attr("data-usd").value.strip
-         puts "#{index + 1}. #{name} $#{price}"
+         #puts "#{index + 1}. #{name}-$#{price}"
+         coin_hash = {:name => name, :price => price,:index => index + 1 , :link => link}
        }
-
   end
 end
